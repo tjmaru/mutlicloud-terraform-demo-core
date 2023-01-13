008082eacd11d887db74c5548aa0b24e1c01174c
tjmaru <30507792+tjmaru@users.noreply.github.com>
2023-01-13 16:09:00 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,8 +1,6 @@
 terraform {
-  required_version = "~> 0.12.6"
+  required_version = ">= 0.12.6"
   required_providers {
-    aws     = ">= 2.60.0"
-    azurerm = ">= 2.0"
     google  = ">= 3.21"
   }
 }
